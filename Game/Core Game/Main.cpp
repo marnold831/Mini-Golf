74ac95119682c3daeb405f949a1c2d0ef0f18bca
marnold831 <marnold0831@hotmail.com>
2020-02-18 15:56:46 +0000

    Addition physx files and project changed to x64

--- Game/Core Game/Main.cpp
+++ Game/Core Game/Main.cpp
@@ -1,10 +1,28 @@
 #include "../../Common/Window.h"
 #include "TestEnviroment.h"
 
+#include "PxPhysics.h"
+#include "PxScene.h"
+#include "PxRigidDynamic.h"
+#include "PxShape.h"
+#include "PxPhysicsAPI.h"
+#include "PxPhysXConfig.h"
+#include "foundation/PxMemory.h"
+#include "extensions/PxDefaultStreams.h"
+#include "extensions/PxDefaultErrorCallback.h"
+
 using namespace NCL;
 using namespace CSC8503;
+using namespace physx;
 
 int main() {
+	static PxDefaultErrorCallback gDefaultErrorCallback;
+	static PxDefaultAllocator gDefaultAllocatorCallback;
+
+	auto mFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gDefaultAllocatorCallback,
+		gDefaultErrorCallback);
+	if (!mFoundation)
+		return -1;
 	Window*w = Window::CreateGameWindow("Alpha Test Window", 1280, 720);
 
 	if (!w->HasInitialised()) {
